af87e2be4c8a5a1194c80fae63da6da810529b5d
hugompt <hugomsfh@hotmail.com>
2022-03-15 00:42:12 +0100

    CHANGELOG - Added a hover tooltip for the copy but it's not working 100% needs a revision on the JS

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ let iconsEye = document.querySelectorAll('.icon2');
 let iconsCopy = document.querySelectorAll('.icon3');
 let tooltipText = document.querySelectorAll('.tooltipClicktext');
 let tooltipEyeText = document.querySelectorAll('.tt2');
+let tooltipCopyText = document.querySelectorAll('.tt3');
+let upload = document.querySelector('.upload');
 
 //Function to change visibility of the card text
 //after the user clicked on the eye logo
@@ -14,11 +16,11 @@ for (let i = 0; i < cards.length; i++) {
         if(cardText[i].style.visibility == 'hidden'){
             cardText[i].style.visibility = 'visible';
             iconsEye[i].style.backgroundImage = "url('./imgs/visibility_black_24dp.svg')";
-            tooltipEyeText[i].textContent = "Click me to hide the text on the card"
+            tooltipEyeText[i].textContent = "Click me to HIDE the text on the card"
         }else{
             cardText[i].style.visibility = 'hidden';
             iconsEye[i].style.backgroundImage = "url('./imgs/visibility_off_black_24dp.svg')";
-            tooltipEyeText[i].textContent = "Click me to show the text on the card"
+            tooltipEyeText[i].textContent = "Click me to SHOW the text on the card"
         }
     });
 }
@@ -28,11 +30,20 @@ for (let i = 0; i < cards.length; i++) {
 //and also tooltip for visual confirmation
 for (let i = 0; i < cards.length; i++) {
     iconsCopy[i].addEventListener("click", function() {
+        tooltipCopyText[i].style.visibility = 'hidden';
+        tooltipCopyText[i].style.opacity = 0;
+     
         tooltipText[i].style.visibility = 'visible';
         tooltipText[i].style.opacity = 1;
-        navigator.clipboard.writeText((cardTitle[i].textContent + cardText[i].textContent).replace(/[ \t]{1,}/g, ' '));});
+        navigator.clipboard.writeText((cardTitle[i].textContent +
+             cardText[i].textContent).replace(/[ \t]{1,}/g, ' '));
+    });
         iconsCopy[i].addEventListener('mouseleave',function(){
             tooltipText[i].style.visibility = 'hidden';
             tooltipText[i].style.opacity = 0;
-        })
-    }
+        });
+}
+
+upload.addEventListener("click", function(){
+    $('#filez').click();
+})
