804d4911eeba2e20a674e9e72dc1422ef03fe541
hugompt <hugomsfh@hotmail.com>
2022-03-14 18:55:16 +0100

    CHANGELOG - Made copy to clipboard function work, just needs a bit of revision to trim the extra spaces from the string;

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 // Global Variables
 let cards = document.querySelectorAll('.card');
+let cardTitle = document.querySelectorAll('.cardTitle');
 let cardText = document.querySelectorAll('.cardText');
-let icons = document.querySelectorAll('.icon2');
-
+let iconsEye = document.querySelectorAll('.icon2');
+let iconsCopy = document.querySelectorAll('.icon3');
 
 //Function to change visibility of the card text
 //after the user clicked on the eye logo
 for (let i = 0; i < cards.length; i++) {
-    icons[i].addEventListener("click", function() {
+    iconsEye[i].addEventListener("click", function() {
         if(cardText[i].style.visibility == 'hidden'){
             cardText[i].style.visibility = 'visible';
-            icons[i].style.backgroundImage = "url('./imgs/visibility_black_24dp.svg')";
+            iconsEye[i].style.backgroundImage = "url('./imgs/visibility_black_24dp.svg')";
         }else{
             cardText[i].style.visibility = 'hidden';
-            icons[i].style.backgroundImage = "url('./imgs/visibility_off_black_24dp.svg')";
+            iconsEye[i].style.backgroundImage = "url('./imgs/visibility_off_black_24dp.svg')";
         }
     });
+}
+
+//Function to copy text from card
+//after user clicked on the clipboard icon
+for (let i = 0; i < cards.length; i++) {
+    iconsCopy[i].addEventListener("click", function() {
+        navigator.clipboard.writeText(cardTitle[i].textContent + cardText[i].textContent);});
 }
